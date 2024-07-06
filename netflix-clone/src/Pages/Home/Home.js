878006b7e4748cdc07dc0df24ec651ccf7af137c
Legese1919 <bayushlegese1919@gmail.com>
2024-07-06 08:17:45 -0400

    banner

--- netflix-clone/src/Pages/Home/Home.js
+++ netflix-clone/src/Pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Banner from "../../Components/Banner/banner";
-// import RowList from "../../Components/Rows/Rowlist/Rowlist";
+import RowList from "../../Components/Rows/Rowlist/Rowlist";
 
 
 function Home() {
@@ -10,7 +10,7 @@ function Home() {
 		<div>
 			<Header />
 			<Banner />
-			{/* <RowList /> */}
+			<RowList />
 			<Footer />
 		</div>
 	);
