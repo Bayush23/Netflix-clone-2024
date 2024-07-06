a2f012827e5d190fe4b67f7e77dbd32a5333a0a1
Legese1919 <bayushlegese1919@gmail.com>
2024-07-06 08:30:29 -0400

    axios and requests

--- netflix-clone/src/Utils/requests.js
+++ netflix-clone/src/Utils/requests.js
@@ -2,14 +2,14 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const requests = {
 	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
-	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, // Netflix Originals
-	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`, // Top rated movies
-	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`, // Action movies
-	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, // Comedy movies
-	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`, // Horror movies
-	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, // Romance movies
-	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, // Documentaries
-	fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`, // Popular TV shows
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`, 
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`, 
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`, 
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`, 
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`, 
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`, 
+	fetchTvShow: `/tv/popular?api_key=${API_KEY}&language=en-US&page=1`, 
 };
 
 export default requests;
